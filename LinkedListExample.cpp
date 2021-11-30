12a899307c7f79fef08209a25da02a3ac4c33ce5
S1mphiwe <95218165+S1mphiwe@users.noreply.github.com>
2021-11-30 17:28:13 +0100

    GUI First Commit

---
+++ LinkedListExample.cpp
@@ -0,0 +1,18 @@
+// LinkedList Example : Console Application
+#include "stdafx.h"
+#include "MyList.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[]){
+	List L;//list declaration
+	Init (L); //list intialisation
+	Insert ("musa", L); Insert ("jane", L); Insert ("smile", L); ShowData (L);//Function calls to insert and display data
+	Delete ("jane",L);ShowData (L); // function calls to delete and display data on screen
+
+
+	system("pause");
+	return 0;
+}
+
